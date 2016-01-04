e799d94dbb22b00b6c7ba3f8aeb2d7208899cb13
manhtai <vo@manhtai.com>
2016-01-04 17:07:51 +0700

    Add requirements, use default Flask server as web server

--- app.py
+++ app.py
@@ -1,28 +1,28 @@
 from flask import Blueprint
 main = Blueprint('main', __name__)
- 
+
 import json
 from engine import RecommendationEngine
- 
+
 import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
- 
+
 from flask import Flask, request
- 
+
 @main.route("/<int:user_id>/ratings/top/<int:count>", methods=["GET"])
 def top_ratings(user_id, count):
     logger.debug("User %s TOP ratings requested", user_id)
     top_ratings = recommendation_engine.get_top_ratings(user_id,count)
     return json.dumps(top_ratings)
- 
+
 @main.route("/<int:user_id>/ratings/<int:movie_id>", methods=["GET"])
 def movie_ratings(user_id, movie_id):
     logger.debug("User %s rating requested for movie %s", user_id, movie_id)
     ratings = recommendation_engine.get_ratings_for_movie_ids(user_id, [movie_id])
     return json.dumps(ratings)
- 
- 
+
+
 @main.route("/<int:user_id>/ratings", methods = ["POST"])
 def add_ratings(user_id):
     # get the ratings from the Flask POST request object
@@ -32,15 +32,15 @@ def add_ratings(user_id):
     ratings = map(lambda x: (user_id, int(x[0]), float(x[1])), ratings_list)
     # add them to the model using then engine API
     recommendation_engine.add_ratings(ratings)
- 
-    return json.dumps(ratings)
- 
- 
-def create_app(spark_context, dataset_path):
-    global recommendation_engine 
 
-    recommendation_engine = RecommendationEngine(spark_context, dataset_path)    
-    
+    return json.dumps(ratings)
+
+
+def create_app(spark_context, dataset_path):
+    global recommendation_engine
+
+    recommendation_engine = RecommendationEngine(spark_context, dataset_path)
+
     app = Flask(__name__)
     app.register_blueprint(main)
-    return app 
+    return app
